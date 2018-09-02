e2a12e8abe0b95ced84bb0fd5df059a0c279eadb
James Johnson <johnsojr@gmail.com>
2018-09-02 16:46:24 -0500

    init

---
+++ _next/a8e5c10c-a9bd-4441-8b5b-21308f4b35d4/page/about.js
@@ -0,0 +1,1 @@
+module.exports=__NEXT_REGISTER_PAGE("/about",function(){return{page:webpackJsonp([4],{222:function(e,t,n){e.exports=n(223)},223:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var u=n(2),a=n.n(u),o=n(78);t.default=function(){return a.a.createElement(o.a,null,a.a.createElement("p",null,"This is the about page"))}}},[222]).default}});
